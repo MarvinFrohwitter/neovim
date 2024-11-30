a94852fbc6ba964bfefa32aaa8ffa85e2a3133c4
Marvin Frohwitter <marvin.frohwitter@web.de>
2024-11-30 05:25:13 +0100

    fix: vimtex compilation and preview by using defaults.

--- init.lua
+++ init.lua
@@ -11,16 +11,6 @@ require("autoexec")
 require("init")
 
 vim.cmd([[
-    let g:vimtex_view_method          = 'zathura'
-    let g:vimtex_view_general_viewer  = 'evince'
-    let g:vimtex_view_general_options = '--unique file:@pdf\#src:@line@tex'
-    let g:vimtex_compiler_method      = 'latexrun'
-    let g:livepreview_previewer       = 'zathura'
-    let g:livepreview_engine          = 'pdflatex'
-    let g:livepreview_use_biber       = 1
-]])
-
-vim.cmd([[
     if !exists('g:undotree_WindowLayout')
         let g:undotree_WindowLayout = 2
     endif
--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -3,9 +3,20 @@ return {
 	{
 		"lervag/vimtex",
 		ft = { "tex" },
+		init = function()
+			vim.g.vimtex_view_method = "zathura"
+			-- vim.g.vimtex_view_general_viewer = "evince"
+			-- vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
+			-- vim.g.vimtex_compiler_method = "latexrun"
+		end,
 	},
 	{
 		"xuhdev/vim-latex-live-preview",
 		ft = { "tex" },
+		init = function()
+			vim.g.livepreview_previewer = "zathura"
+			vim.g.livepreview_engine = "pdflatex"
+			vim.g.livepreview_use_biber = 1
+		end,
 	},
 }
