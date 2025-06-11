23db8665cd60bb8867a5400f87abd7f55446a026
Marvin Frohwitter <marvin.frohwitter@web.de>
2025-06-11 18:23:49 +0200

    feat: Try out of the lsp folder for configuring lsp clients.

---
+++ lsp/luals.lua
@@ -0,0 +1,34 @@
+return {
+	-- cmd = {
+	-- 	"lua-language-server",
+	-- },
+	-- filetypes = {
+	-- 	"lua",
+	-- },
+	-- root_markers = {
+	-- 	".luarc.json",
+	-- 	".luarc.jsonc",
+	-- },
+
+	-- settings = {
+	-- 	Lua = {
+	-- 		runtime = {
+	-- 			-- LuaJIT in the case of Neovim
+	-- 			version = "LuaJIT",
+	-- 			path = vim.split(package.path, ";"),
+	-- 		},
+	-- 		diagnostics = {
+	-- 			-- Get the language server to recognize the `vim` global
+	-- 			globals = { "vim" },
+	-- 		},
+	-- 		workspace = {
+	-- 			checkThirdParty = false,
+	-- 			-- Make the server aware of Neovim runtime files
+	-- 			library = {
+	-- 				[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+	-- 				[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+	-- 			},
+	-- 		},
+	-- 	},
+	-- },
+}
